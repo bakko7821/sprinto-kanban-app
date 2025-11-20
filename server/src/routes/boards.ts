a85e4655111915ac8f447629896d4db6a08c7cf9
Dmitry Eshman <bakko7821@gmail.com>
2025-11-20 12:43:24 +0300

    commit 12.15.0

--- server/src/routes/boards.ts
+++ server/src/routes/boards.ts
@@ -43,16 +43,48 @@ router.get("/:id", authMiddleware, async(req: AuthRequest, res: Response) => {
         const board = await Board.findByPk(id)
 
         if (!board) {
-            return res.status(404).json({ message: "Пользователь не найден" });
+            return res.status(404).json({ message: "Доска не найдена" });
         }
 
         res.json(board);
 
     } catch (error: unknown) {
         console.error(error);
-        res.status(500).json({ message: "Ошибка при создании новой доски" });
+        res.status(500).json({ message: "Ошибка при получении доски" });
     }
 
+})
+
+router.put("/:id", authMiddleware, async(req: AuthRequest, res: Response) => {
+    const userId = (req.user as any)?.id;
+    
+    if (!userId) {
+      return res.status(400).json({ message: "Некорректный токен" });
+    }
+
+    try {
+        const id = Number(req.params.id)
+
+        const board = await Board.findByPk(id) 
+
+        if (!board) {
+            return res.status(404).json({ message: "Задача не найдена" });
+        }
+
+        const { name, backgroundImage, isPrivate} = req.body;
+
+        await board.update({
+            name,
+            backgroundImage,
+            isPrivate
+        });
+
+        res.json(board);
+
+    } catch (error: unknown) {
+        console.error(error);
+        res.status(500).json({ message: "Ошибка при изменении доски" });
+    }
 })
 
 export default router
